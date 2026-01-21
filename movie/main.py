66d9a8a9c87bbeab87e9b168643bc15451e6e438
OriErez3 <orierez3@gmail.com>
2026-01-21 13:29:31 -0500

    Created the table of movies that will be viewable to the user. Also created the show function, in order to make it easy to see the list.

--- movie/main.py
+++ movie/main.py
@@ -1,27 +1,43 @@
 import typer 
 from rich import print
-from rich import console
-from rich import table
+from rich.console import Console
+from rich.table import Table
 
-
+console = Console()
 
 app = typer.Typer(name="Movie")
 
 @app.command(short_help="Add a new movie")
 def add(movie: str, watched:bool, rating: int):
     typer.echo(f"Adding {movie}")
+    show()
 
 @app.command(short_help="Delete a movie")
 def remove(movie: str):
     typer.echo(f"Removing {movie}")
+    show()
     
 @app.command(short_help="View all movies")
-def view(movie: str):
-    typer.echo("Here are the movies you watched: ")
+def show():
+    testTable = [("Interstellar", True, 10), ("Marty Supreme", True, 7)]
+    console.print('[red]Test[/red]')
+    table = Table(show_header=True,header_style="bold red")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Movie", min_width=20)
+    table.add_column("Watched?", min_width=10)
+    table.add_column("Rating", min_width=10)
+
+    for idx, movie in enumerate(testTable, start=1):
+        is_watched = "✔" if True == 2 else "Ⅹ"
+        table.add_row(str(idx), movie[0], str(movie[1]), str(movie[2]))
+    console.print(table)
+
+
 
 @app.command(short_help="Update a movie")
 def update(movie: str, watched: bool, rating: int):
     typer.echo(f"Updating {movie}")
+    show()
 
 
 
